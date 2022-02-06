d15d79de716c02600578864d78b6ea1d9afab29c
Matthias Stieger <matthiass413@gmail.com>
2022-02-06 17:10:08 +0100

    implement repository class for addresses

---
+++ JobManagement/DataAccessLayer/Repositories/AddressRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Context;
+using DataAccessLayer.DataTransferObjects;
+
+namespace DataAccessLayer.Repositories
+{
+    public class AddressRepository
+    {
+        private static string ConnectionString { get; set; }
+
+        public AddressRepository(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public AddressDto GetAddressById(int id)
+        {
+            using (var context = new JobManagementContext(ConnectionString))
+            {
+                var address = context.Addresses.Find(id);
+
+                if (address != null)
+                    return new AddressDto()
+                    {
+                        Id = address.Id,
+                        Street = address.Street,
+                        StreetNumber = address.StreetNumber,
+                        Zip = address.Zip,
+                        Country = address.Country,
+                        City = address.City
+                    };
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public void AddNewAddress(AddressDto addressDto)
+        {
+            using (var context = new JobManagementContext(ConnectionString))
+            {
+                context.Addresses.Add(addressDto);
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteAddressByDto(AddressDto addressDto)
+        {
+            using (var context = new JobManagementContext(ConnectionString))
+            {
+                context.Addresses.Remove(addressDto);
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateAddressByDto(AddressDto addressDto)
+        {
+            using (var context = new JobManagementContext(ConnectionString))
+            {
+                context.Addresses.Update(addressDto);
+                context.SaveChanges();
+            }
+        }
+    }
+}
