f5a26a50bc633d4d8c7d5e6b0093008ecc81ac6f
billeberhard <bill.eberhard193e@gmail.com>
2022-03-01 17:07:09 +0100

    Article View und Orcer View implementiert

--- JobManagement/PresentationLayer_/MVVM/ViewModel/MainViewModel.cs
+++ JobManagement/PresentationLayer_/MVVM/ViewModel/MainViewModel.cs
@@ -4,20 +4,54 @@ namespace PresentationLayer.MVVM.ViewModel
 {
     class MainViewModel : ObservableObject
     {
-        public HomeViewModel HomeVM(get; set;)
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand CustomerViewCommand { get; set; }
+
+        public RelayCommand ArticleViewCommand { get; set; }
+        public RelayCommand OrderViewCommand { get; set; }
+
+
+        public HomeViewModel HomeVM{get; set;}
+        public CustomerViewModel CustomerVM { get; set; }
+        public ArticleViewModel ArticleVM { get; set; }
+        public OrderViewModel OrderVM { get; set; }
+
         private object _currentView;
+       
         public object CurrentView
         {
-            get { return CurrentView; }
+            get { return _currentView; }
             set
             {
-                CurrentView = value;
+                _currentView = value;
                 OnPropertyChanged();
             }
         }
         public MainViewModel()
         {
             HomeVM = new HomeViewModel();
+            CustomerVM = new CustomerViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o => 
+            { 
+                CurrentView = HomeVM;
+            });
+
+            CustomerViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = CustomerVM;
+            });
+            ArticleViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = ArticleVM;
+            });
+            OrderViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = OrderVM;
+            });
         }
    
     }
