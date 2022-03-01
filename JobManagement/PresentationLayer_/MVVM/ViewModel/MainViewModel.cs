ac1db98d4ffeb934982fb97382ef459788f5d56f
billeberhard <bill.eberhard193e@gmail.com>
2022-03-01 15:46:45 +0100

    Views umstellen

--- JobManagement/PresentationLayer_/MVVM/ViewModel/MainViewModel.cs
+++ JobManagement/PresentationLayer_/MVVM/ViewModel/MainViewModel.cs
@@ -9,9 +9,20 @@ namespace PresentationLayerWPF.MVVM.ViewModel
 {
     class MainViewModel : ObservableObject
     {
+        public HomeViewModel HomeVM(get; set;)
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return CurrentView; }
+            set
+            {
+                CurrentView = value;
+                OnPropertyChanged();
+            }
+        }
         public MainViewModel()
         {
-
+            HomeVM = new HomeViewModel();
         }
    
     }
