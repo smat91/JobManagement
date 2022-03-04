cf691e0588001a55095e12a789882654f191ad8c
miladfakiry <78559990+miladfakiry@users.noreply.github.com>
2022-03-04 21:38:04 +0100

    Add PositionConnection class / Add Method in this class

---
+++ JobManagement/PresentationLayer_/MVVM/ViewModel/Connections/PositionConnection.cs
@@ -0,0 +1,41 @@
+﻿using DataAccessLayer.Interfaces;
+using DataAccessLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentationLayer.MVVM.ViewModel.Connections
+{
+    public class PositionConnection
+    {
+        private readonly PositionRepository positionRepository_;
+
+        public PositionConnection(string connectionString)
+        {
+            positionRepository_ = new PositionRepository(connectionString);
+        }
+
+        public IPosition GetPositionById(int id)
+        {
+           var position = positionRepository_.GetPositionById(id);
+           return position;
+        }
+
+        public void AddNewPosition(IPosition position)
+        {
+            positionRepository_.AddNewPosition(position);
+        }
+
+        public void DeletePositionByDto(IPosition position)
+        {
+           positionRepository_.DeletePositionByDto(position);
+        }
+
+        public void UpdatePositionByDto(IPosition position)
+        {
+           positionRepository_.UpdatePositionByDto(position);
+        }
+    }
+}
