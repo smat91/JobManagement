fc2808ad994c0cc30160cf258fd770ba3f9469c0
miladfakiry <78559990+miladfakiry@users.noreply.github.com>
2022-03-06 21:04:50 +0100

    Added horizontal dynamic Columnrows into HomeView

--- JobManagement/PresentationLayer_/MVVM/View/HomeView.xaml.cs
+++ JobManagement/PresentationLayer_/MVVM/View/HomeView.xaml.cs
@@ -1,15 +1,46 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows.Controls;
 
 namespace PresentationLayer.MVVM.View
 {
     /// <summary>
     /// Interaktionslogik für HomeView.xaml
     /// </summary>
+    
     public partial class HomeView : UserControl
     {
         public HomeView()
         {
             InitializeComponent();
+            AddDataGridColumn();
+            
+
         }
+
+        public static int Quarter(DateTime date)
+        {
+            return (date.Month + 2) / 3;
+        }
+        private void AddDataGridColumn()
+        {
+            DataGridTextColumn textColumn = new DataGridTextColumn();
+            textColumn.Header = "Kategorie";
+            //textColumn.Binding = new Binding("FirstName");
+            DataGridJahresvergleich.Columns.Add(textColumn);
+
+            //Aktuelles Datum holen und retour rechnen, um alle Quartale zu erhalten von den letzten 3 Jahren (dynamisch)
+            for(int i = 0; i < 12; i++)
+            {
+                int quartal = Quarter(DateTime.Today.AddMonths(-i*3));
+                DataGridTextColumn textColumn2 = new DataGridTextColumn();
+                textColumn2.Header = DateTime.Today.AddMonths(-i * 3).Year + " Q" + quartal;
+                //textColumn.Binding = new Binding("FirstName");
+                DataGridJahresvergleich.Columns.Add(textColumn2);
+            }
+            
+            
+        }
+
     }
+
 }
