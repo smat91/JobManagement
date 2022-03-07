d48ebd512420440d00eeba76f534f1c26ad632df
Matthias Stieger <matthiass413@gmail.com>
2022-03-07 12:34:10 +0100

    Add StatisticsConnection and some test stuf in MainWidow

---
+++ JobManagement/PresentationLayer_/MVVM/ViewModel/Connections/StatisticsConnection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Repositories;
+
+namespace PresentationLayer.MVVM.ViewModel.Connections
+{
+    public class StatisticsConnection
+    {
+        public static List<ExpandoObject> GetStatisticData()
+        {
+            var statistics = StatisticsRepository.GetStatisticData();
+            return statistics;
+        }
+    }
+}
--- JobManagement/PresentationLayer_/MainWindow.xaml.cs
+++ JobManagement/PresentationLayer_/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using DataAccessLayer.Models;
 using PresentationLayer.MVVM.ViewModel.Connections;
 
 namespace PresentationLayer
@@ -11,6 +12,54 @@ namespace PresentationLayer
         public MainWindow()
         {
             InitializeComponent();
+
+            //var testGroup1 = new ItemGroup
+            //{
+            //    Name = "Elektronik"
+            //};
+            //ItemGroupConnection.AddNewItemGroup(testGroup1);
+
+            //var testGroup2 = new ItemGroup
+            //{
+            //    Name = "Aktive Bauteile",
+            //    ParentItemGroup = testGroup1
+            //};
+            //ItemGroupConnection.AddNewItemGroup(testGroup2);
+
+            //var testGroup3 = new ItemGroup
+            //{
+            //    Name = "Passive Bauteile",
+            //    ParentItemGroup = testGroup1
+            //};
+            //ItemGroupConnection.AddNewItemGroup(testGroup3);
+
+            //var testGroup4 = new ItemGroup
+            //{
+            //    Name = "Wiederstände",
+            //    ParentItemGroup = testGroup3
+            //};
+            //ItemGroupConnection.AddNewItemGroup(testGroup4);
+
+            //var testItem1 = new Item
+            //{
+            //    Name = "Drahtwiderstand 10Ohm",
+            //    Group = testGroup4,
+            //    Price = 0.05m,
+            //    Vat = 5.80m
+            //};
+            //ItemConnection.AddNewItem(testItem1);
+
+            //var testItem2 = new Item
+            //{
+            //    Name = "Drahtwiderstand 20Ohm",
+            //    Group = testGroup4,
+            //    Price = 0.05m,
+            //    Vat = 5.80m
+            //};
+            //ItemConnection.AddNewItem(testItem2);
+
+            var test = ItemGroupConnection.GetItemsWithLevel();
+            var test1 = StatisticsConnection.GetStatisticData();
         }
     }
 }
