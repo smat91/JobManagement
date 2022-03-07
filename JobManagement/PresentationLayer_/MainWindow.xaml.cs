e0eba88728198bdd798b70692e6f680dc44b8517
Matthias Stieger <matthiass413@gmail.com>
2022-03-07 22:29:46 +0100

    Remove unneeded test code

--- JobManagement/PresentationLayer_/MainWindow.xaml.cs
+++ JobManagement/PresentationLayer_/MainWindow.xaml.cs
@@ -12,64 +12,6 @@ namespace PresentationLayer
         public MainWindow()
         {
             InitializeComponent();
-
-            //var testGroup1 = new ItemGroup
-            //{
-            //    Name = "Elektronik"
-            //};
-            //ItemGroupConnection.AddNewItemGroup(testGroup1);
-
-            //var testGroup2 = new ItemGroup
-            //{
-            //    Name = "Aktive Bauteile",
-            //    ParentItemGroup = testGroup1
-            //};
-            //ItemGroupConnection.AddNewItemGroup(testGroup2);
-
-            //var testGroup3 = new ItemGroup
-            //{
-            //    Name = "Passive Bauteile",
-            //    ParentItemGroup = testGroup1
-            //};
-            //ItemGroupConnection.AddNewItemGroup(testGroup3);
-
-            //var testGroup4 = new ItemGroup
-            //{
-            //    Name = "Wiederstände",
-            //    ParentItemGroup = testGroup3
-            //};
-            //ItemGroupConnection.AddNewItemGroup(testGroup4);
-
-            //var testItem1 = new Item
-            //{
-            //    Name = "Drahtwiderstand 10Ohm",
-            //    Group = testGroup4,
-            //    Price = 0.05m,
-            //    Vat = 5.80m
-            //};
-            //ItemConnection.AddNewItem(testItem1);
-
-            //var testItem2 = new Item
-            //{
-            //    Name = "Drahtwiderstand 20Ohm",
-            //    Group = testGroup4,
-            //    Price = 0.05m,
-            //    Vat = 5.80m
-            //};
-            //ItemConnection.AddNewItem(testItem2);
-
-
-            var item1 = ItemConnection.GetItemById(1);
-            var item2 = ItemConnection.GetItemById(2);
-
-            item1.Price = 0.06m;
-            item2.Price = 0.07m;
-
-            ItemConnection.UpdateItemByDto(item1);
-            ItemConnection.UpdateItemByDto(item2);
-
-            //var test = ItemGroupConnection.GetItemsWithLevel();
-            //var test1 = StatisticsConnection.GetStatisticData();
         }
     }
 }
