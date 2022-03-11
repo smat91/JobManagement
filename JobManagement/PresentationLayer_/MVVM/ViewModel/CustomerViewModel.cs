a3eedffd41523e827ff4e62530be7c0f2c2ffdfc
Matthias Stieger <matthiass413@gmail.com>
2022-03-11 01:37:26 +0100

    Add data grids

--- JobManagement/PresentationLayer_/MVVM/ViewModel/CustomerViewModel.cs
+++ JobManagement/PresentationLayer_/MVVM/ViewModel/CustomerViewModel.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessLayer.DataAccessConnection;
+using BusinessLayer.DataTransferObjects;
 
 namespace PresentationLayer.MVVM.ViewModel
 {
     internal class CustomerViewModel
     {
+        public List<CustomerDto> CustomerDtoList { get; set; }
+
+        public CustomerViewModel()
+        {
+            Customer customer = new Customer();
+            CustomerDtoList = customer.GetAllCustomers();
+        }
     }
 }
--- JobManagement/PresentationLayer_/MVVM/ViewModel/OrderViewModel.cs
+++ JobManagement/PresentationLayer_/MVVM/ViewModel/OrderViewModel.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessLayer.DataAccessConnection;
+using BusinessLayer.DataTransferObjects;
 
 namespace PresentationLayer.MVVM.ViewModel
 {
     internal class OrderViewModel
     {
+        public List<OrderDto> OrderDtoList { get; set; }
+
+        public OrderViewModel()
+        {
+            Order order = new Order();
+            OrderDtoList = order.GetAllOrders();
+        }
     }
 }
