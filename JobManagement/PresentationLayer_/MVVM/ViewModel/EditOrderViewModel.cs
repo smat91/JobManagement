45685c942506bc60aaa39a633ae062138f2eecc2
miladfakiry <78559990+miladfakiry@users.noreply.github.com>
2022-03-12 19:03:15 +0100

    Filled EditOrderViewModel with Code

--- JobManagement/PresentationLayer_/MVVM/ViewModel/EditOrderViewModel.cs
+++ JobManagement/PresentationLayer_/MVVM/ViewModel/EditOrderViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Windows;
 using BusinessLayer.DataAccessConnection;
 using BusinessLayer.DataTransferObjects;
@@ -9,7 +10,27 @@ using PresentationLayer.Core;
 
 namespace PresentationLayer.MVVM.ViewModel
 {
-    class EditOrderViewModel : ObservableObject
+    class EditOrderViewModel : NewOrderViewModel
     {
+        public EditOrderViewModel() : base()
+        {
+            Order order = new Order();
+            order_ = order.GetOrderById(MainViewModel.SelectedId);
+        }
+
+        private void Save()
+        {
+            Order order = new Order();
+            if (DataCheck())
+            {
+                order.UpdateOrderByDto(order_);
+            }
+            else
+            {
+                MessageBox.Show("Einige Felder sind unvollständig\noder keine Positionen vorhanden!");
+            }
+        }
+
     }
 }
+
