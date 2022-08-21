509d9625fde2bbc02c1c420b96322081d53a9725
Matthias Stieger <matthiass413@gmail.com>
2022-08-21 14:54:41 +0200

    Add new migration for customer number

---
+++ JobManagement/DataAccessLayer/Migrations/20220821123300_AddCustomerNumber.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class AddCustomerNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomerId",
+                table: "InvoiceRequest");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerNumber",
+                table: "InvoiceRequest",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerNumber",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomerNumber",
+                table: "InvoiceRequest");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerNumber",
+                table: "Customers")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "CustomersHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", null);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CustomerId",
+                table: "InvoiceRequest",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
