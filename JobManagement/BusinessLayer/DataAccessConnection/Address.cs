9d3ca89d0ccbbd2219fbba71935082733cb43c09
Matthias Stieger <matthiass413@gmail.com>
2022-09-04 11:36:58 +0200

    Add base repository to address

--- JobManagement/BusinessLayer/DataAccessConnection/Address.cs
+++ JobManagement/BusinessLayer/DataAccessConnection/Address.cs
@@ -16,35 +16,35 @@ namespace BusinessLayer.DataAccessConnection
 
         public AddressDto GetAddressById(int id)
         {
-            var address = addressRepository_.GetAddressById(id);
+            var address = addressRepository_.GetSingleById(id);
             return new AddressDto(address);
         }
 
         public List<AddressDto> GetAddressesBySearchTerm(string searchTerm)
         {
-            var addresses = addressRepository_.GetAddressesBySearchTerm(searchTerm);
+            var addresses = addressRepository_.GetBySearchTerm(searchTerm);
             return AddressDto.AddressListToAddressDtoList(addresses);
         }
 
         public List<AddressDto> GetAllAddresses()
         {
-            var addresses = addressRepository_.GetAllAddresses();
+            var addresses = addressRepository_.GetAll();
             return AddressDto.AddressListToAddressDtoList(addresses);
         }
 
         public void AddNewAddress(AddressDto address)
         {
-            addressRepository_.AddNewAddress(AddressDto.AddressDtoToAddress(address));
+            addressRepository_.Add(AddressDto.AddressDtoToAddress(address));
         }
 
         public string DeleteAddressByDto(AddressDto address)
         {
-            return addressRepository_.DeleteAddressByDto(AddressDto.AddressDtoToAddress(address));
+            return addressRepository_.Delete(AddressDto.AddressDtoToAddress(address));
         }
 
         public void UpdateAddressByDto(AddressDto address)
         {
-            addressRepository_.UpdateAddressByDto(AddressDto.AddressDtoToAddress(address));
+            addressRepository_.Update(AddressDto.AddressDtoToAddress(address));
         }
     }
 }
--- JobManagement/DataAccessLayer/Repositories/AddressRepository.cs
+++ JobManagement/DataAccessLayer/Repositories/AddressRepository.cs
@@ -10,82 +10,83 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DataAccessLayer.Repositories
 {
-    public class AddressRepository
+    public class AddressRepository : BaseRepository<Address>
     {
-        public Address GetAddressById(int id)
-        {
-            using (var context = new JobManagementContext())
-            {
-                var address = context.Addresses.Find(id);
-                return address;
-            }
-        }
+        public override string TableName => "Address";
+        //public Address GetAddressById(int id)
+        //{
+        //    using (var context = new JobManagementContext())
+        //    {
+        //        var address = context.Addresses.Find(id);
+        //        return address;
+        //    }
+        //}
 
-        public List<Address> GetAddressesBySearchTerm(string searchTerm)
-        {
-            List<Address> addressList = new List<Address>();
-            Search search = new Search();
+        //public List<Address> GetAddressesBySearchTerm(string searchTerm)
+        //{
+        //    List<Address> addressList = new List<Address>();
+        //    Search search = new Search();
 
-            using (var context = new JobManagementContext())
-            {
-                context.Addresses
-                    .AsEnumerable()
-                    .Where(address => search.EvaluateSearchTerm(searchTerm, address))
-                    .ToList()
-                    .ForEach(address => addressList.Add(address));
-            }
+        //    using (var context = new JobManagementContext())
+        //    {
+        //        context.Addresses
+        //            .AsEnumerable()
+        //            .Where(address => search.EvaluateSearchTerm(searchTerm, address))
+        //            .ToList()
+        //            .ForEach(address => addressList.Add(address));
+        //    }
 
-            return addressList;
-        }
+        //    return addressList;
+        //}
 
-        public List<Address> GetAllAddresses()
-        {
-            using (var context = new JobManagementContext())
-            {
-                List<Address> addressesList = new List<Address>();
+        //public List<Address> GetAllAddresses()
+        //{
+        //    using (var context = new JobManagementContext())
+        //    {
+        //        List<Address> addressesList = new List<Address>();
 
-                context.Addresses
-                    .ToList()
-                    .ForEach(address => addressesList.Add(address));
+        //        context.Addresses
+        //            .ToList()
+        //            .ForEach(address => addressesList.Add(address));
 
-                return addressesList;
-            }
-        }
+        //        return addressesList;
+        //    }
+        //}
 
-        public void AddNewAddress(Address address)
-        {
-            using (var context = new JobManagementContext())
-            {
-                context.Addresses.Add(address);
-                context.SaveChanges();
-            }
-        }
+        //public void AddNewAddress(Address address)
+        //{
+        //    using (var context = new JobManagementContext())
+        //    {
+        //        context.Addresses.Add(address);
+        //        context.SaveChanges();
+        //    }
+        //}
 
-        public string DeleteAddressByDto(Address address)
-        {
-            using (var context = new JobManagementContext())
-            {
-                context.Addresses.Remove(address);
+        //public string DeleteAddressByDto(Address address)
+        //{
+        //    using (var context = new JobManagementContext())
+        //    {
+        //        context.Addresses.Remove(address);
 
-                try
-                {
-                    context.SaveChanges();
-                    return "Datensatz erfolgreich gelöscht";
-                }
-                catch (DbUpdateException e)
-                {
-                    return "Datensatz konnte nicht gelöscht werden.\nBitte zuerst Datensätze erntfernen in denen der Datensatz verwendet wird.";
-                }
-            }
-        }
+        //        try
+        //        {
+        //            context.SaveChanges();
+        //            return "Datensatz erfolgreich gelöscht";
+        //        }
+        //        catch (DbUpdateException e)
+        //        {
+        //            return "Datensatz konnte nicht gelöscht werden.\nBitte zuerst Datensätze erntfernen in denen der Datensatz verwendet wird.";
+        //        }
+        //    }
+        //}
 
-        public void UpdateAddressByDto(Address address)
-        {
-            using (var context = new JobManagementContext())
-            {
-                context.Addresses.Update(address);
-                context.SaveChanges();
-            }
-        }
+        //public void UpdateAddressByDto(Address address)
+        //{
+        //    using (var context = new JobManagementContext())
+        //    {
+        //        context.Addresses.Update(address);
+        //        context.SaveChanges();
+        //    }
+        //}
     }
 }
